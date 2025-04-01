02412d868344cc536961e747316fe51f45e60307
leonquist429 <leonquist429@gmail.com>
2025-04-01 12:25:39 -0700

    change: channel name

--- src/bot/library/startHandler.ts
+++ src/bot/library/startHandler.ts
@@ -11,7 +11,7 @@ export const startHandler = async (msg: any) => {
 
 You can subscribe the below channels to get the latest pairs and marketcap alerts:
 
-<a href="https://t.me/+NOClaexcINZhM2Ex">10K Channel</a>  |  <a href="https://t.me/+xMgIa2TAnj8yMDdh">20K Channel</a>  |  <a href="https://t.me/+JjBn1pl9BcQ2MWUx">50K Channel</a>`,
+<a href="https://t.me/+NOClaexcINZhM2Ex">1K Channel</a>  |  <a href="https://t.me/+xMgIa2TAnj8yMDdh">2K Channel</a>  |  <a href="https://t.me/+JjBn1pl9BcQ2MWUx">5K Channel</a>`,
       {
         parse_mode: "HTML",
         disable_web_page_preview: true,
