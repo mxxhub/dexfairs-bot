04a162ca64b97b1ca908941ff185d4c89b47d201
leonquist429 <leonquist429@gmail.com>
2025-04-03 11:38:17 -0700

    [fix]: issue

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -4,9 +4,9 @@ import { deleteDaily } from "../controller/deleteweekly";
 
 export const initAll = async () => {
   try {
-    await getNewPairInfoHandler();
-    await startMarketCapMonitoring();
-    await deleteDaily();
+    getNewPairInfoHandler();
+    startMarketCapMonitoring();
+    deleteDaily();
   } catch (err) {
     console.log("Error in initAll:", err);
   }
