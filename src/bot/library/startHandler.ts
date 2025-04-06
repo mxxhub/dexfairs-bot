7fd6d5b77d58c6e6dfcfcbb9acace5204d6916e5
leonquist429 <leonquist429@gmail.com>
2025-04-06 02:35:45 -0700

    [change]: channel id and name

--- src/bot/library/startHandler.ts
+++ src/bot/library/startHandler.ts
@@ -11,9 +11,15 @@ export const startHandler = async (msg: any) => {
 
 You can subscribe the below channels to get the latest pairs and marketcap alerts:
 
+New Pair Channels
+
 <b>ETH: </b> <a href="https://t.me/+ilwP12dcgigyNjcx">20K Channel</a> | <a href="https://t.me/+LOHQP3Qg8xNjY2Ex">50K Channel</a> | <a href="https://t.me/+WewXFlgk-YxjY2Vh">1M Channel</a>
 <b>BASE: </b> <a href="https://t.me/+bKcKEcaoFpM4NzQx">20K Channel</a> | <a href="https://t.me/+b8qP8TQEqIs4ZmQx">50K Channel</a> | <a href="https://t.me/+-_rK7_av-utjNDUx">1M Channel</a>
-<b>BSC: </b> <a href="https://t.me/+MLiF_hAf6dRjNTY5">20K Channel</a> | <a href="https://t.me/+d5L0vdbKwZhjNDEx">50K Channel</a> | <a href="https://t.me/+c9Rsacxt72QwZmYx">1M Channel</a>`,
+<b>BSC: </b> <a href="https://t.me/+MLiF_hAf6dRjNTY5">20K Channel</a> | <a href="https://t.me/+d5L0vdbKwZhjNDEx">50K Channel</a> | <a href="https://t.me/+c9Rsacxt72QwZmYx">1M Channel</a>
+
+All Time Low Channels
+
+<a href="https://t.me/+kcqGeisF0BZmMDdh">ETH Channel</a> | <a href="https://t.me/+gqHjLpvCMF05Yjkx">BSC Channel</a> | <a href="https://t.me/+W9LKpQL3m205YjAx">BASE Channel</a>`,
       {
         parse_mode: "HTML",
         disable_web_page_preview: true,
