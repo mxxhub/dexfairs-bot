124cb9fb39654ccce8d1963bfcf0fedaef7e60a9
leonquist429 <leonquist429@gmail.com>
2025-04-11 18:40:08 +0800

    [add]: check scam part code.

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -5,3 +5,16 @@ export interface IPair {
   createdAt: Date;
   flag: boolean;
 }
+
+export interface Token {
+  is_honeypot: string;
+  is_mintable: string;
+  can_take_back_ownership: string;
+  hidden_owner: string;
+  slippage_modifiable: string;
+  buy_tax: string;
+  sell_tax: string;
+  is_blacklisted: string;
+  cannot_sell_all: string;
+  transfer_pausable: string;
+}
