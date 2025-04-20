0d52db70f48bd420723a9374adda22afb7fd939c
leonquist429 <leonquist429@gmail.com>
2025-04-20 19:42:48 +0800

    [feat]: global interface and check scam part.

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -7,14 +7,20 @@ export interface IPair {
 }
 
 export interface Token {
-  is_honeypot: string;
-  is_mintable: string;
-  can_take_back_ownership: string;
-  hidden_owner: string;
-  slippage_modifiable: string;
-  buy_tax: string;
-  sell_tax: string;
-  is_blacklisted: string;
-  cannot_sell_all: string;
-  transfer_pausable: string;
+  honeypotResult: {
+    isHoneypot: boolean;
+  };
+  simulationResult: {
+    buyTax: number;
+    sellTax: number;
+  };
+  token: {
+    totalHolders: number;
+  };
+  pair: {
+    liquidity: number;
+  };
+  contractCode: {
+    openSource: boolean;
+  };
 }
--- src/utils/checkScam.ts
+++ src/utils/checkScam.ts
@@ -1,17 +1,17 @@
 import { Token } from "../@types/global";
 
-export const checkScam = (token: Token) => {
-  console.log("token of checking: ", token);
-  return (
-    token?.is_honeypot === "1" ||
-    token?.is_mintable === "1" ||
-    token?.can_take_back_ownership === "1" ||
-    token?.hidden_owner === "1" ||
-    token?.slippage_modifiable === "1" ||
-    parseFloat(token?.buy_tax) >= 10 ||
-    parseFloat(token?.sell_tax) >= 10 ||
-    token?.is_blacklisted === "1" ||
-    token?.cannot_sell_all === "1" ||
-    token?.transfer_pausable === "1"
-  );
+export const checkScam = (data: Token) => {
+  try {
+    console.log("token of checking: ", data);
+    return (
+      data.honeypotResult.isHoneypot === true ||
+      data.simulationResult.buyTax > 10 ||
+      data.simulationResult.sellTax > 10 ||
+      data.token.totalHolders < 10 ||
+      data.pair.liquidity < 1 ||
+      data.contractCode.openSource === false
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
